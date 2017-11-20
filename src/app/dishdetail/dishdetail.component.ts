27d87f9359b210893def36458922a8b2056a1f30
Nan Liu <nanliuca2outlook.com>
2017-11-20 12:48:29 -0800

    menu component added

---
+++ src/app/dishdetail/dishdetail.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-dishdetail',
+  templateUrl: './dishdetail.component.html',
+  styleUrls: ['./dishdetail.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class DishdetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
